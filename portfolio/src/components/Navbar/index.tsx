2a4749be9003c73f97a722f7f163bffe4ba50348
Luiz Lopes ᴮᴿ <71521080+luizlopes12@users.noreply.github.com>
2023-09-06 14:49:48 -0300

    feat: navbar responsive structure

--- portfolio/src/components/Navbar/index.tsx
+++ portfolio/src/components/Navbar/index.tsx
@@ -1,6 +1,9 @@
 import "./style.scss";
 import brandLogo from "../../assets/img/brand-logo.svg";
 import configIcon from "../../assets/img/config-icon.svg";
+import mobileLine1 from "../../assets/img/mobile-line1.svg";
+import mobileLine2 from "../../assets/img/mobile-line2.svg";
+import mobileLine3 from "../../assets/img/mobile-line3.svg";
 import Translator from "../I18n/Translator";
 import { useTranslation } from "react-i18next";
 import { useRef, useState } from "react";
@@ -8,18 +11,27 @@ import { useRef, useState } from "react";
 const Navbar = () => {
   const { t } = useTranslation();
 
-  const configsTooltipBody = useRef<HTMLDivElement>(null);
-  const configsTooltipContainer = useRef<HTMLDivElement>(null);
+  const configsTooltipBodyRef = useRef<HTMLDivElement>(null);
+  const configsTooltipContainerRef = useRef<HTMLDivElement>(null);
+  const mobileMenuContainerRef = useRef<HTMLDivElement>(null);
+  const navbarContainerRef = useRef<HTMLDivElement>(null);
 
   const handleShowConfigs = () => {
-    if (configsTooltipBody.current && configsTooltipContainer.current) {
-      configsTooltipBody.current.classList.toggle("-active");
-      configsTooltipContainer.current.classList.toggle("-active");
+    if (configsTooltipBodyRef.current && configsTooltipContainerRef.current) {
+      configsTooltipBodyRef.current.classList.toggle("-active");
+      configsTooltipContainerRef.current.classList.toggle("-active");
+    }
+  };
+
+  const handleShowMobileMenu = () => {
+    if (mobileMenuContainerRef.current && navbarContainerRef.current) {
+      mobileMenuContainerRef.current.classList.toggle("-active");
+      navbarContainerRef.current.classList.toggle("-active");
     }
   };
 
   return (
-    <section className="navbar__container">
+    <section className="navbar__section-container">
       <section className="navbar__section">
         <a href="#aboutme">
             <figure className="brandlogo__figure">
@@ -30,8 +42,13 @@ const Navbar = () => {
             />
             </figure>
         </a>
-        <nav className="navbar__container">
-          <div className="configs__tooltip-container" ref={configsTooltipContainer}>
+        <div className="mobile__menu-container" ref={mobileMenuContainerRef} onClick={handleShowMobileMenu}>
+          <img src={mobileLine1} className="mobile__menu-line1" />
+          <img src={mobileLine2} className="mobile__menu-line2" />
+          <img src={mobileLine3} className="mobile__menu-line3" />
+        </div>
+        <nav className="navbar__container" ref={navbarContainerRef}>
+          <div className="configs__tooltip-container" ref={configsTooltipContainerRef}>
             <div
               className="configs__tooltip-header"
               onClick={handleShowConfigs}
@@ -45,32 +62,32 @@ const Navbar = () => {
             </div>
             <div
               className="configs__tooltip-body"
-              ref={configsTooltipBody}
+              ref={configsTooltipBodyRef}
             ></div>
           </div>
           <ul className="navbar__list">
             <li className="navbar__item">
-              <a href="#aboutme">
+              <a href="#aboutme" onClick={handleShowMobileMenu}>
                 <Translator path="aboutMe" />
               </a>
             </li>
             <li className="navbar__item">
-              <a href="#techlologies">
+              <a href="#techlologies" onClick={handleShowMobileMenu}>
                 <Translator path="technologies" />
               </a>
             </li>
             <li className="navbar__item">
-              <a href="#experiences">
+              <a href="#experiences" onClick={handleShowMobileMenu}>
                 <Translator path="experiences" />
               </a>
             </li>
             <li className="navbar__item">
-              <a href="#projects">
+              <a href="#projects" onClick={handleShowMobileMenu}>
                 <Translator path="projects" />
               </a>
             </li>
             <li className="navbar__item">
-              <a href="#contact">
+              <a href="#contact" onClick={handleShowMobileMenu}>
                 <Translator path="contact" />
               </a>
             </li>
