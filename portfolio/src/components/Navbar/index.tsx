ca9354f5d93e65f7278be517f4d4ca05543b3b7d
Luiz Lopes ᴮᴿ <71521080+luizlopes12@users.noreply.github.com>
2023-09-05 23:18:59 -0300

    feat: minor adjusts on z index from navbar list and config tooltip

--- portfolio/src/components/Navbar/index.tsx
+++ portfolio/src/components/Navbar/index.tsx
@@ -1,69 +1,84 @@
-import './style.scss'
-import brandLogo from '../../assets/img/brand-logo.svg'
-import configIcon from '../../assets/img/config-icon.svg'
-import Translator from '../I18n/Translator'
-import { useTranslation } from 'react-i18next'
-import { useRef } from 'react'
+import "./style.scss";
+import brandLogo from "../../assets/img/brand-logo.svg";
+import configIcon from "../../assets/img/config-icon.svg";
+import Translator from "../I18n/Translator";
+import { useTranslation } from "react-i18next";
+import { useRef, useState } from "react";
 
 const Navbar = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
-  const configsTooltipBody = useRef<HTMLDivElement>(null)
+  const configsTooltipBody = useRef<HTMLDivElement>(null);
+  const configsTooltipContainer = useRef<HTMLDivElement>(null);
+
   const handleShowConfigs = () => {
-    if(configsTooltipBody.current) {
-      configsTooltipBody.current.classList.toggle('-active')
+    if (configsTooltipBody.current && configsTooltipContainer.current) {
+      configsTooltipBody.current.classList.toggle("-active");
+      configsTooltipContainer.current.classList.toggle("-active");
     }
-  }
-  
-  return (
-    <section className='navbar__section'>
-        <figure className="brandlogo__figure">
-            <img src={brandLogo} alt="Logo 'Luiz Lopes'" title={t('logoWelcomeTitle')}/>
-        </figure>
-        <nav className='navbar__container'>
-            <div className="configs__tooltip-container">
-                <div className="configs__tooltip-header" onClick={handleShowConfigs}>
-                    <figure className="configs__tooltip-icon">
-                        <img src={configIcon} alt={t('settings')} />
-                    </figure>
-                    <div className="configs__tooltip-text">
-                        <Translator path='settings'/>
-                    </div>
-                </div>
-                <div className="configs__tooltip-body" ref={configsTooltipBody}>
-                    
-                </div>
-            </div>
-            <ul className="navbar__list">
-                <li className="navbar__item">
-                    <a href="#aboutme">
-                        <Translator path='aboutMe'/>
-                    </a>
-                </li>
-                <li className="navbar__item">
-                    <a href="#techlologies">
-                        <Translator path='technologies'/>
-                    </a>
-                </li>
-                <li className="navbar__item">
-                    <a href="#experiences">
-                        <Translator path='experiences'/>
-                    </a>
-                </li>
-                <li className="navbar__item">
-                    <a href="#projects">
-                        <Translator path='projects'/>
-                    </a>
-                </li>
-                <li className="navbar__item">
-                    <a href="#contact">
-                        <Translator path='contact'/>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    </section>
-  )
-}
+  };
 
-export default Navbar
+  return (
+    <section className="navbar__container">
+      <section className="navbar__section">
+        <a href="#aboutme">
+            <figure className="brandlogo__figure">
+            <img
+                src={brandLogo}
+                alt="Logo 'Luiz Lopes'"
+                title={t("logoWelcomeTitle")}
+            />
+            </figure>
+        </a>
+        <nav className="navbar__container">
+          <div className="configs__tooltip-container" ref={configsTooltipContainer}>
+            <div
+              className="configs__tooltip-header"
+              onClick={handleShowConfigs}
+            >
+              <figure className="configs__tooltip-icon">
+                <img src={configIcon} alt={t("settings")} />
+              </figure>
+              <div className="configs__tooltip-text">
+                <Translator path="settings" />
+              </div>
+            </div>
+            <div
+              className="configs__tooltip-body"
+              ref={configsTooltipBody}
+            ></div>
+          </div>
+          <ul className="navbar__list">
+            <li className="navbar__item">
+              <a href="#aboutme">
+                <Translator path="aboutMe" />
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a href="#techlologies">
+                <Translator path="technologies" />
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a href="#experiences">
+                <Translator path="experiences" />
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a href="#projects">
+                <Translator path="projects" />
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a href="#contact">
+                <Translator path="contact" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </section>
+  );
+};
+
+export default Navbar;
