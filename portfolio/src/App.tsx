f3dcbc74cf6de6d863e995a31d8a583ca8ce4de0
Luiz Lopes ᴮᴿ <71521080+luizlopes12@users.noreply.github.com>
2023-09-13 17:57:44 -0300

    feat: add vercel analytics

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,9 +1,11 @@
 import './assets/sass/main.scss'
 import HomePage from './pages/HomePage/HomePage'
+import { Analytics } from '@vercel/analytics/react';
 function App() {
   return (
     <>
       <HomePage/>
+      <Analytics />
     </>
   )
 }
