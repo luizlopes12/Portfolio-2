81168761b3f506a8eb815aa6e6505d43598f6306
Luiz Lopes ᴮᴿ <71521080+luizlopes12@users.noreply.github.com>
2023-09-13 17:49:57 -0300

    chore: removing swiper imports to not effect the deploy

--- portfolio/src/components/Projects/index.tsx
+++ portfolio/src/components/Projects/index.tsx
@@ -1,8 +1,8 @@
 import './style.scss'
 // import { useTranslation } from 'react-i18next'
 import Translator from '../I18n/Translator'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCoverflow, Autoplay } from 'swiper/modules';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import { EffectCoverflow, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
