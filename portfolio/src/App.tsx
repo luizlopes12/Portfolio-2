6385d2cb458e832ca85be19fba35cf3948c696b7
Luiz Lopes ᴮᴿ <71521080+luizlopes12@users.noreply.github.com>
2023-09-13 17:59:30 -0300

    feat: removing vercel analytics

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,11 +1,9 @@
 import './assets/sass/main.scss'
 import HomePage from './pages/HomePage/HomePage'
-import { Analytics } from '@vercel/analytics/react';
 function App() {
   return (
     <>
       <HomePage/>
-      <Analytics />
     </>
   )
 }
